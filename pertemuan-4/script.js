6cdf6355e57e52e038ae8ecc18d0d4a416c8db1b
kayy98 <rafka0714@student.abudzar.scd.id>
2025-09-26 08:57:41 +0700

    tugas-18

---
+++ pertemuan-4/script.js
@@ -0,0 +1,40 @@
+// alert("Sudah terintegrasi");
+
+console.log("test")
+
+let kresek1 = "Bakwan";
+let kresek2 = 100;
+let kresek3 = 20;
+
+let kresekHasil = kresek2+kresek3
+
+console.log(kresek1);
+console.log(kresek2);
+console.log(kresekHasil);
+
+//!Tipe Data
+let umur = 20;
+console.log(umur);
+
+umur ="dua puluh";
+console.log(umur)
+
+let angkaBesar =3456789098765434567898765433334n;
+
+ let contohSatu="diapit oleh petik atas 2" + kresek1 + "ini tambahan tulisan";
+ let contohDua ='diapit petik 1';
+ let contohTiga = 'menggunakan backtik ${kresek1} ini tambahan tulisan';
+
+console.log(contohSatu);
+console.log(contohTiga);
+
+let hidup = 1;
+let mati = 0;
+
+let gayung = true;
+console.log(gayung);
+console.log(typeof gayung);
+
+
+let age = prompt("berpakah umur anda" , 100)
+alert("umur anda adalah" + age);
